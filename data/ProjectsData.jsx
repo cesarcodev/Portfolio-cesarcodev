4a27302b13c171fff7ade9e1eb00ed85f71548ee
CesarCodev <cesarce735@gmail.com>
2024-02-07 16:59:29 -0400

    Cambiar la ruta de la imágenes del proyecto y actualización del CV

--- data/ProjectsData.jsx
+++ data/ProjectsData.jsx
@@ -45,7 +45,7 @@ export const ProjectsData = [
         title: " Gestión y Seguimiento del Despacho de Fertilizantes - Diproagro C.A.",
         description: 'Diproagro C.A. es una empresa agropecuaria venezolana que usa un sistema web para gestionar el despacho de fertilizantes. El sistema web está hecho con PHP, Javascript, HTML y CSS, y fue desarrollado por un grupo de programadores, entre los que me encuentro.',
         technologies: [TAGS.HTML,TAGS.CSS,TAGS.JS,TAGS.PHP,TAGS.SYMFONY],
-        image:"projects/DPA.webp",
+        image:"Projects/DPA.webp",
         height:'md:h-16',
         github: '',
         link: ''
@@ -57,7 +57,7 @@ export const ProjectsData = [
         photos: ["clavellinas/portada.png", "clavellinas/1.png", "clavellinas/2.png", "clavellinas/3.png", "clavellinas/4.png", "clavellinas/5.png"],
         description: 'Gestión y Seguimiento de Evaluaciones es un sistema web que evalúa y reporta el desempeño de los trabajadores de Diproagro C.A. El sistema web está hecho con PHP, Javascript, HTML y CSS. Desarrollé soluciones innovadoras para este sistema junto con mi equipo de trabajo.',
         technologies: [TAGS.HTML,TAGS.CSS,TAGS.JS,TAGS.PHP],
-        image:"projects/EVAL.webp",
+        image:"Projects/EVAL.webp",
         height:'md:h-16',
         github: '',
         link: ''
@@ -69,7 +69,7 @@ export const ProjectsData = [
         photos: ["condesalud/portada.png", "condesalud/1l.png", "condesalud/2l.png", "condesalud/3l.png"],
         description: 'Emporium es un sistema de e-commerce con landing page. Participé en la creación de soluciones de este sistema junto con el grupo DIGITAL PARTNERS SOLUTIONS, C.A. El sistema se adapta a diversos negocios y mercados, y está hecho con Angular y PHP.',
         technologies: [TAGS.HTML,TAGS.ANGULAR,TAGS.PHP,TAGS.SYMFONY],
-        image:"projects/Emporium.webp",
+        image:"Projects/Emporium.webp",
         height:'md:h-auto',
         github: '',
         link: ''
@@ -81,7 +81,7 @@ export const ProjectsData = [
         photos: ["condesalud/portada.png", "condesalud/1l.png", "condesalud/2l.png", "condesalud/3l.png"],
         description: 'Wayá es un sistema de e-business B2B2C. Desarrollé este sistema junto con el grupo DIGITAL PARTNERS SOLUTIONS, C.A. Wayá cuenta con varios módulos que le dan una ventaja competitiva en el mercado online. Estuvo bajo mi cargo todas las soluciones frontend, usando Angular como framework.',
         technologies: [TAGS.HTML,TAGS.ANGULAR,TAGS.PHP],
-        image:"projects/WAYA.webp",
+        image:"Projects/WAYA.webp",
         height:'md:h-auto',
         github: '',
         link: ''
